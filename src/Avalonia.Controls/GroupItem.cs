6f74f26bf764d1bf5afd6692c22187455c20370f
pdmartin101 <75180820+pdmartin101@users.noreply.github.com>
2020-12-26 20:45:17 +0000

    Changed to GroupItem<T>

--- src/Avalonia.Controls/GroupItem.cs
+++ src/Avalonia.Controls/GroupItem.cs
@@ -1,15 +1,17 @@
+using System;
 using Avalonia.Controls.Generators;
 using Avalonia.Controls.Metadata;
 using Avalonia.Controls.Mixins;
 using Avalonia.Controls.Presenters;
 using Avalonia.Input;
+using Avalonia.Styling;
 
 namespace Avalonia.Controls
 {
     /// <summary>
     /// A selectable item in a <see cref="GroupItem"/>.
     /// </summary>
-    public class GroupItem : ItemsControl
+    public abstract class GroupItem : ItemsControl
     {
         public GroupItem()
         {
@@ -29,36 +31,36 @@ namespace Avalonia.Controls
         /// <summary>
         /// Defines the <see cref="VirtualizationMode"/> property.
         /// </summary>
-        public static readonly StyledProperty<ItemVirtualizationMode> VirtualizationModeProperty =
-            ItemsPresenter.VirtualizationModeProperty.AddOwner<GroupItem>();
+        //public static readonly StyledProperty<ItemVirtualizationMode> VirtualizationModeProperty =
+        //    ItemsPresenter.VirtualizationModeProperty.AddOwner<GroupItem>();
 
         /// <summary>
         /// Gets or sets the virtualization mode for the items.
         /// </summary>
-        public ItemVirtualizationMode VirtualizationMode
-        {
-            get { return GetValue(VirtualizationModeProperty); }
-            set { SetValue(VirtualizationModeProperty, value); }
-        }
+        //public ItemVirtualizationMode VirtualizationMode
+        //{
+        //    get { return GetValue(VirtualizationModeProperty); }
+        //    set { SetValue(VirtualizationModeProperty, value); }
+        //}
         static GroupItem()
         {
             PressedMixin.Attach<GroupItem>();
             FocusableProperty.OverrideDefaultValue<GroupItem>(true);
-//            VirtualizationModeProperty.OverrideDefaultValue<GroupItem>(ItemVirtualizationMode.Simple);
+            //            VirtualizationModeProperty.OverrideDefaultValue<GroupItem>(ItemVirtualizationMode.Simple);
         }
 
     }
 
-    public class GroupListBoxItem : GroupItem
+    public class GroupItem<T> : GroupItem, IStyleable where T : class, IControl, new()
     {
-        public GroupListBoxItem()
+        public GroupItem()
         {
         }
         protected override IItemContainerGenerator CreateTopItemContainerGenerator()
         {
             if (Presenter is ItemsPresenter ip)
                 ip.VirtualizationMode = ItemVirtualizationMode.Smooth;
-            return new ItemContainerGenerator<ListBoxItem>(
+            return new ItemContainerGenerator<T>(
                 this,
                 ListBoxItem.ContentProperty,
                 ListBoxItem.ContentTemplateProperty);
@@ -69,11 +71,13 @@ namespace Avalonia.Controls
         /// Initializes static members of the <see cref="GroupListBoxItem"/> class.
         /// </summary>
 
-        static GroupListBoxItem()
+        Type IStyleable.StyleKey => typeof(GroupItem);
+
+        static GroupItem()
         {
-            PressedMixin.Attach<GroupListBoxItem>();
-            FocusableProperty.OverrideDefaultValue<GroupListBoxItem>(true);
-//            VirtualizationModeProperty.OverrideDefaultValue<GroupListBoxItem>(ItemVirtualizationMode.Simple);
+            //            PressedMixin.Attach<GroupListBoxItem>();
+            //            FocusableProperty.OverrideDefaultValue<GroupListBoxItem>(true);
+            //            VirtualizationModeProperty.OverrideDefaultValue<GroupListBoxItem>(ItemVirtualizationMode.Simple);
         }
 
     }
